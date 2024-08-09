d0b647fd6c1d96ca79f7eb04900409d5dafef0ad
jndamito <josiasndam35@gmail.com>
2024-08-09 10:22:40 -0400

    hard-coded input to fake XSS

--- src/main/java/servlets/BuyBooksServlet.java
+++ src/main/java/servlets/BuyBooksServlet.java
@@ -52,7 +52,8 @@ public class BuyBooksServlet extends HttpServlet {
             for (Book book : books) {
                 String bCode = HTMLUtils.escapeHtml(book.getBarcode()); // Using HTMLUtils to escape HTML
                 String bName = HTMLUtils.escapeHtml(book.getName()); // Using HTMLUtils to escape HTML
-                String bAuthor = HTMLUtils.escapeHtml(book.getAuthor()); // Using HTMLUtils to escape HTML
+                String bAuthor = "Author";
+                //String bAuthor = HTMLUtils.escapeHtml(book.getAuthor()); // Using HTMLUtils to escape HTML
                 double bPrice = book.getPrice();
                 int bAvl = book.getQuantity();
                 i = i + 1;
